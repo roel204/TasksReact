5e14bbe94b263f61e14ce6fe27fc861688598c72
roelh <roelhoogendoorn01369@gmail.com>
2024-01-27 13:21:42 +0100

    Build still not working.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ const defaultConfig = require('tailwindcss/defaultConfig');
 
 module.exports = {
   ...defaultConfig,
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+      './src/index.css'
+  ],
   theme: {
     extend: {},
   },
